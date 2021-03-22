4713f4d4a1a4d1574c8bd370f05ccdb5b28d6d83
Mateusz Kacprzak <mrscraber@gmail.com>
2021-03-22 21:41:02 +0100

    S6: Add archive image tool

--- tools/utils.py
+++ tools/utils.py
@@ -1,3 +1,6 @@
+from zipfile import ZipFile
+from io import BytesIO
+
 def fix_url_http(url: str) -> str:
     if not url.startswith("http"):
         return "http://" + url
@@ -10,3 +13,12 @@ def get_resource_full_url(base_url: str, resource_url: str) -> str:
     if resource_url.startswith("/") and base_url.endswith("/"):
         return base_url + resource_url[1:]
     return base_url + resource_url
+
+def archive_bytes_stream(bytes_data):
+    buffer = BytesIO()
+
+    with ZipFile(buffer, 'w') as zip_file:
+        for count, data in enumerate(bytes_data):
+            zip_file.writestr(count, data)
+
+    return buffer.getvalue()
